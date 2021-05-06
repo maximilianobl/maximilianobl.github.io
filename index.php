2a5c0fc492f4a888559667f44aa9af024609f19a
maczy00 <blascomaxi@gmail.com>
2021-05-06 11:57:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Mi segunda pagina web </title>
 </head>
 <body>
-<h1 align="center" >Mi Primera pagina web </h1>
+<h1 align="center" >Mi segunda pagina web PHP</h1>
 <hr>
 <p>Esto tan sencillo es una verdadera página web, aunque le falta el contenido, 
 pero todo llegará.</p>
